689aa4520f913514ade2a1334c3916a531165afd
GodDandies <gd_dev@outlook.jp>
2017-07-03 16:03:36 +0900

    全体的に微修正

---
+++ js/jquery.magnific-popup.parameter.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    // 単体画像用
+    $('.popup-image').magnificPopup({
+        type: 'image',
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
+
+    // 複数画像グルーピング用
+    $('.popup-image-group').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true,
+            titleSrc: function (item) {
+                return item.el.attr('title') + ' &middot; <a class="image-source-link" href="' + item.el.attr('data-source') + '" target="_blank">image source</a>';
+            }
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function (element) {
+                return element.find('img');
+            }
+        }
+
+    });
+});
--- js/swiper.parameter.js
+++ js/swiper.parameter.js
@@ -1,9 +1,11 @@
-var swiper = new Swiper('.slideshow', {
-    calculateHeight: true,
-	pagination: '.swiper-pagination',
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-	paginationClickable: true,
-	speed: 400,
-    effect: "slide",
+$(document).ready(function () {
+    var swiper = new Swiper('.slideshow', {
+        calculateHeight: true,
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        speed: 400,
+        effect: "slide",
+    });
 });
