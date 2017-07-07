483a4a15103056ab2ec07d513c1c07b19d8319fd
GodDandies <gd_dev@outlook.jp>
2017-07-07 17:51:19 +0900

    微修正

--- js/twitterWidgetChecker.js
+++ js/twitterWidgetChecker.js
@@ -3,7 +3,7 @@ function changeTwitterWidgetDesign　()　 {
     var $twitter_widget_contents = $twitter_widget.contents();
 
     if ($twitter_widget.length > 0 && $twitter_widget[0].contentWindow.document.body.innerHTML !== "") {
-        $twitter_widget_contents.find('head').append('<link href="../css/twitter.css" rel="stylesheet" type="text/css">');
+        $twitter_widget_contents.find('head').append('<link href="../css/twitterWidget.css" rel="stylesheet" type="text/css">');
     } else {
         setTimeout(function () {
             changeTwitterWidgetDesign();
